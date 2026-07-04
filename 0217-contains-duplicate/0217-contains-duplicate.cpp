9f5851380f1b6af91d47762271d5c60144fed5ba
Amit Verma <amitverma2k99@gmail.com>
2026-07-04 14:33:49 +0530

    Time: 72 ms (18%), Space: 111.2 MB (8.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for(int i : nums){
-            freq[i]++;
-            if(freq[i]>1){
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.count(num))
                 return true;
-            }
+
+            seen.insert(num);
         }
-        
+
         return false;
-        
     }
 };
