3fbce50e39041c61d540355cf3e13c52f99a597c
Amit Verma <amitverma2k99@gmail.com>
2026-07-17 18:57:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         int MAX = INT_MAX;
+        int MIN = INT_MIN;
+        int sign;
+        if (x<0){
+          sign  = -1;
+        } 
+            
+        else {
+            sign = 1;
+        }
+
+        int reverse_num = 0;
+        long long n = x;
+        if (n < 0) n = -n;
+        while(n>0){
+            int rem = n%10;
+            n = n/10;
+            if (reverse_num > (MAX-rem)/10){
+                return 0;
+            }
+            reverse_num = reverse_num *10 + rem;
+
+        }
+        if(reverse_num == x){
+            return true;
+        }
+        return false;
+        
+    }
+};
