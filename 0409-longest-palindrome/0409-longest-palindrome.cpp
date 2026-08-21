b9c4ebf37e8fa7d5f88a1925b6b9569f2edf2341
Amit Verma <amitverma2k99@gmail.com>
2026-08-21 20:37:05 +0530

    Time: 0 ms (100%), Space: 9.3 MB (67.89%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int count[128] = {0};
+
+        for (char c : s) {
+            count[c]++;
+        }
+
+        int ans = 0;
+        bool hasOdd = false;
+
+        for (int i = 0; i < 128; i++) {
+            ans += (count[i] / 2) * 2;
+
+            if (count[i] % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            ans++;
+        }
+
+        return ans;
+    }
+};
