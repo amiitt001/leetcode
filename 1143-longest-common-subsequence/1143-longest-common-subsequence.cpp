9097dda22636c685ff5d656127970d744f526ab9
Amit Verma <amitverma2k99@gmail.com>
2026-09-23 16:10:14 +0530

    Time: 47 ms (14.02%), Space: 27.7 MB (7.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,36 +1,30 @@
 class Solution
 {
-    public:
-        string text1;
-    string text2;
-    vector<vector < int>> dp;
-
-    int solve(int idx1, int idx2)
+    int lcs(string &s1, string &s2, int i, int j, vector<vector < int>> &dp)
     {
-        if (idx1 < 0 || idx2 < 0)
-        {
+        if (i == s1.length() || j == s2.length())
             return 0;
-        }
-        if (dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
 
-        if (text1[idx1] == text2[idx2])
+        if (dp[i][j] != -1)
         {
-            return dp[idx1][idx2] = 1 + solve(idx1 - 1, idx2 - 1);
+            return dp[i][j];
+        }
+        if (s1[i] == s2[j])
+        {
+            return dp[i][j] = 1 + lcs(s1, s2, i + 1, j + 1, dp);
         }
 
-        return dp[idx1][idx2] = max(solve(idx1 - 1, idx2),
-            solve(idx1, idx2 - 1)
+        return dp[i][j] = max(lcs(s1, s2, i + 1, j, dp),
+            lcs(s1, s2, i, j + 1, dp)
        );
     }
 
-    int longestCommonSubsequence(string text1, string text2)
-    {
-        this->text1 = text1;
-        this->text2 = text2;
-        int n = text1.size();
-        int m = text2.size();
-        dp = vector<vector < int>> (n, vector<int> (m, -1));
-        return solve(text1.size() - 1, text2.size() - 1);
-    }
+    public:
+        int longestCommonSubsequence(string text1, string text2)
+        {
+            int n = text1.length();
+            int m = text2.length();
+            vector<vector < int>> dp(n, vector<int> (m, -1));
+            return lcs(text1, text2, 0, 0, dp);
+        }
 };
